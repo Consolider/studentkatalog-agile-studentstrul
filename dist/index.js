0b5d566b4b8bba57e064791212147127c73b3d53
Ardalan Sale <114071686+ardalansale@users.noreply.github.com>
2025-08-04 16:00:31 +0200

    lägg till student och studentnamn

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const students = [
+    { name: "Klara" },
+    { name: "Amir" },
+    { name: "Francis" },
+];
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+interface Student {
+    name: string;
+}
+
+const students: Student[] = [
+    { name: "Klara" },
+    { name: "Amir" },
+    { name: "Francis" },
+];
