be26eb261ece99108827031d2a75ee6f9c411591
Consolider <christian.spector@protonmail.com>
2025-08-05 15:19:34 +0200

    US3: visa-aktiv-status

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,22 @@
 const students = [
-    { name: "Klara" },
-    { name: "Amir" },
-    { name: "Francis" },
+    { name: "Klara", isActive: true },
+    { name: "Amir", isActive: true },
+    { name: "Francis", isActive: false }
 ];
+const ulList = document.querySelector(".studentlista");
+function renderList() {
+    students.forEach(student => {
+        const li = document.createElement("li");
+        li.className = "student-list_item";
+        if (student.isActive === true) {
+            li.innerHTML = `<p>${student.name}. Aktiv.</p>`;
+        }
+        else {
+            li.innerHTML = `<p>${student.name}. EJ Aktiv.</p>`;
+        }
+        ulList.appendChild(li);
+    });
+}
+renderList();
 export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 interface Student {
     name: string;
+    isActive: boolean;
 }
 
 const students: Student[] = [
-    { name: "Klara" },
-    { name: "Amir" },
-    { name: "Francis" },
+    { name: "Klara", isActive: true },
+    { name: "Amir", isActive: true },
+    { name: "Francis", isActive: false }
 ];
+
+const ulList = document.querySelector(".studentlista") as HTMLDivElement;
+
+function renderList() {
+    students.forEach(student => {
+        const li = document.createElement("li");
+        
+        li.className = "student-list_item"
+        if ( student.isActive === true ) {
+            li.innerHTML = `<p>${student.name}. Aktiv.</p>`;
+        } else {
+            li.innerHTML = `<p>${student.name}. EJ Aktiv.</p>`;
+        }
+        
+        ulList.appendChild(li);
+    });
+}
+
+renderList();
