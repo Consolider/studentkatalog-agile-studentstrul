b965541ee522f08a9fcdd9976cc69968bff5d2c5
Ardalan Sale <114071686+ardalansale@users.noreply.github.com>
2025-08-05 11:14:09 +0200

    US1: Ändrade .studentlista från class till id
    US2: Uppdaterade Student-interface med age och lade till ålder i listan.
    
    Lade även till DOM-manipulation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 interface Student {
     name: string;
+    age: number;
 }
 
 const students: Student[] = [
-    { name: "Klara" },
-    { name: "Amir" },
-    { name: "Francis" },
+    { name: "Klara", age: 35 },
+    { name: "Amir", age: 20 },
+    { name: "Francis", age: 18 },
 ];
 
-// Ingen ändring i koden – bara för commit
+// Dom-manipulation
+const studentContainer = document.querySelector('#studentlista');
+
+if (studentContainer) {
+    students.forEach(student => {
+        const p = document.createElement('p'); // Skapar ett <p>-element för varje elev
+        p.textContent = `${student.name}, ${student.age} år`; // <p>-elementets innehåll sätts till studentens namn ålder.
+        studentContainer.appendChild(p);
+    });
+}
